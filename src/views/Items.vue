6aa72527cc0fc8086607a17366fbd80ec7e20b8c
Oleg <scherbaoleg4@gmail.com>
2021-08-07 23:03:19 +0300

    png favicon & items image preload (paragraph 2 is excluded)

--- src/views/Items.vue
+++ src/views/Items.vue
@@ -8,7 +8,19 @@
     <!-- Card -->
     <div v-for="item in weapon" :key="item.name" class="rounded-lg mt-1 mb-4 md:mb-8 overflow-hidden shadow-lg bg-gray-50 dark:bg-gray-700 transition-colors sm:mx-auto sm:justify-center sm:text-center sm:w-48">
         <router-link class="flex sm:block" v :to="item.href">
-            <img class="w-20 h-20 sm:w-28 sm:h-28 sm:m-auto rounded-xl mx-3 my-3 sm:mb-0" v :src="item.ico" alt="char-portret">
+            <!-- Image loading -->
+            <vue-load-image>
+                <template v-slot:image>
+                    <img class="w-20 h-20 sm:w-28 sm:h-28 sm:m-auto rounded-xl mx-3 my-3 sm:mb-0" v :src="item.ico" alt="item-ico">
+                </template>
+                <template v-slot:preloader>
+                    <div class="w-16 h-16 sm:w-24 sm:h-24 sm:m-auto rounded-2xl mx-5 my-5 sm:mb-1 sm:mt-3 bg-gray-200 dark:bg-gray-600 sm:mx-auto animate-pulse"></div>
+                </template>
+                <template v-slot:error>
+                    <div class="w-16 h-16 sm:w-24 sm:h-24 sm:m-auto rounded-2xl mx-5 my-5 sm:mb-1 sm:mt-3 bg-red-200 dark:bg-red-800 sm:mx-auto animate-pulse text-center">Ошибка</div>
+                </template>
+            </vue-load-image>
+            <!-- /Image -->
             <div class="px-3 py-1 my-auto">
                 <h1 class="font-bold mb-1 sm:mt-1 sm:mb-2 text-xl dark:text-gray-100">{{ item.name }}</h1>
             </div>
@@ -17,23 +29,10 @@
   </div>
 
 
-    <!-- Second section -->
-    <h3 class="pb-2 px-1 sm:text-center sm:pb-5 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:leading-10 dark:text-gray-200">
-        Инвентарь
-    </h3>
 
+<!--
   <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-    <!-- Card -->
-    <div v-for="item in another" :key="item.name" class="rounded-lg mt-1 mb-4 md:mb-8 overflow-hidden shadow-lg bg-gray-50 dark:bg-gray-700 transition-colors sm:mx-auto sm:justify-center sm:text-center sm:w-48">
-        <router-link class="flex sm:block" v :to="item.href">
-            <img class="w-20 h-20 sm:w-24 sm:h-24 sm:mt-3 sm:m-auto rounded-xl mx-3 my-3 sm:mb-0" v :src="item.ico" alt="char-portret">
-            <div class="px-3 py-1 my-auto">
-                <h1 class="font-bold mb-1 sm:mt-1 sm:mb-2 text-xl dark:text-gray-100">{{ item.name }}</h1>
-            </div>
-        </router-link>
-    </div>
-
-    <!-- Preloader mirage -->
+     //Preloader mirage
     <div v-for="n in 4" :key="n" class="rounded-lg mt-1 mb-4 md:mb-8 overflow-hidden shadow-lg bg-gray-50 dark:bg-gray-700 transition-colors sm:mx-auto sm:justify-center sm:text-center sm:w-48">
         <div class="flex sm:block">
             <div class="w-20 h-20 sm:mx-auto rounded-3xl mx-3 my-3 sm:mb-0 sm:my-4 bg-gray-200 dark:bg-gray-600 animate-pulse"></div>
@@ -43,12 +42,14 @@
         </div>
     </div>
   </div>
+-->
 
 </template>
 
 
 <script>
-// TODO lazy image load & сам контент
+// TODO сам контент
+import VueLoadImage from 'vue-load-image'
 
 const weapon = [
   { name: 'Мечи', href: '/sword', ico: "/img/icon_sword.png" },
@@ -58,18 +59,23 @@ const weapon = [
   { name: 'Катализаторы', href: '/catalyst', ico: "/img/icon_catalyst.png" },
 ]
 
+/*
 const another = [
   { name: 'Еда', href: '/food', ico: "/img/icon_food.png" },
   { name: 'Крафты', href: '/blacksmith', ico: "/img/icon_blacksmith.png" },
   { name: 'Алхимия', href: '/alchemy', ico: "/img/icon_alchemy.png" },
 ]
+*/
 
 export default {
   setup() {
     return {
       weapon,
-      another
+      //another
     }
   },
+  components: {
+    'vue-load-image': VueLoadImage
+  }
 }
 </script>

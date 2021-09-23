3f4bad8af5e422dba5049ec867266bfec8f1869f
Oleg <scherbaoleg4@gmail.com>
2021-09-23 15:03:49 +0300

    big images loading spinner

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,8 +5,23 @@
     <!-- Page content (if error == false) -->
     <div v-else class="grid grid-cols-1 md:grid-cols-2">
 
+      <!-- Paimon -->
       <div class="rounded-lg mt-1 mb-4 md:mb-8 overflow-hidden mx-auto justify-center text-center">
-          <img loading="eager" class="h-72 sm:h-96 m-auto rounded-xl mt-4" src="/img/paimon-portrait.png" alt="paimon-portret">
+          <!-- Image loading -->
+            <vue-load-image>
+              <template v-slot:image>
+                  <img loading="eager" class="h-72 sm:h-96 m-auto rounded-xl mt-4" src="/img/paimon-portrait.png" alt="paimon-portret">
+              </template>
+              <template v-slot:preloader> 
+                  <div class="h-72 sm:h-96 mx-auto sm:mt-4 mb-4 sm:mb-0 pt-28">
+                    <svg class="mx-auto animate-spin text-spin text-gray-400 h-10 w-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" id="spin"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
+                  </div>
+              </template>
+              <template v-slot:error>
+                  <div class="h-72 sm:h-96 mx-auto mt-3 pt-28"></div>
+              </template>
+            </vue-load-image>
+          <!-- /Image -->
       </div>
 
 
@@ -59,11 +74,13 @@
 
 <script>
 import axios from 'axios'
+import VueLoadImage from 'vue-load-image'
 import Error from '@/components/Error.vue'
 
 
 export default {
   components: {
+    'vue-load-image': VueLoadImage,
     Error
   },
 
--- src/views/Character.vue
+++ src/views/Character.vue
@@ -4,7 +4,9 @@
     <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
 
       <div class="rounded-lg mt-1 mb-4 md:mb-8 overflow-hidden mx-auto justify-center text-center">
-          <div class="h-80 sm:h-96 m-auto rounded-xl mt-3"></div>
+          <div class="h-80 sm:h-96 mx-auto mt-3 pt-28">
+              <svg class="mx-auto text-gray-400 h-10 w-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" id="spin"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle></svg>
+          </div>
       </div>
 
       <div class="px-3 pb-2 rounded-lg mb-4 md:mb-8 overflow-hidden text-lg text-gray-900 dark:text-gray-200 shadow-lg bg-gray-50 dark:bg-gray-700 transition-colors sm:m-4 justify-center">
@@ -37,7 +39,21 @@
     <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
 
       <div class="rounded-lg mt-1 mb-4 md:mb-8 overflow-hidden mx-auto justify-center text-center">
-          <img loading="eager" class="h-80 sm:h-96 m-auto rounded-xl mt-3" v :src="'https://sushicat.pp.ua/api'+character.portrait.path" alt="char-portret">
+          <!-- Image loading -->
+            <vue-load-image>
+              <template v-slot:image>
+                  <img loading="eager" class="h-80 sm:h-96 mx-auto mt-3" v :src="'https://sushicat.pp.ua/api'+character.portrait.path" alt="char-portret">
+              </template>
+              <template v-slot:preloader> 
+                  <div class="h-80 sm:h-96 mx-auto mt-3 pt-28">
+                    <svg class="mx-auto animate-spin text-spin text-gray-400 h-10 w-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" id="spin"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
+                  </div>
+              </template>
+              <template v-slot:error>
+                  <div class="h-80 sm:h-96 mx-auto mt-3 pt-28"></div>
+              </template>
+            </vue-load-image>
+          <!-- /Image -->
       </div>
 
       <div class="px-3 pb-2 rounded-lg mb-4 md:mb-8 overflow-hidden text-lg text-gray-900 dark:text-gray-200 shadow-lg bg-gray-50 dark:bg-gray-700 transition-colors sm:m-4 justify-center">
@@ -77,14 +93,16 @@
 
 
 <script>
-// Сделать норм оповещение о неправильном url
+// TODO Сделать норм оповещение о неправильном url
 import axios from 'axios'
+import VueLoadImage from 'vue-load-image'
 import Error from '@/components/Error.vue'
 import { StarIcon } from '@heroicons/vue/solid'
 
 
 export default {
   components: {
+    'vue-load-image': VueLoadImage,
     StarIcon,
     Error
   },

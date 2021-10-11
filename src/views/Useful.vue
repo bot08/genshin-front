12aedfef58f3f76f8142e30ef841b2b12f4f9cb0
Oleg <scherbaoleg4@gmail.com>
2021-10-11 19:08:23 +0300

    useful page upd

--- src/views/Useful.vue
+++ src/views/Useful.vue
@@ -1,45 +1,110 @@
 <template>
-        <!-- 
-        <div class="grid grid-cols-1 lg:grid-cols-2 mt-1">
-            <div class="px-3 pb-2 rounded-lg mb-4 md:mb-8 overflow-hidden text-gray-900 dark:text-gray-200 shadow-lg bg-gray-50 dark:bg-gray-700 transition-colors sm:mx-4 justify-center">
-                <table class="table-auto border-collapse border w-full mt-2">
-                    <tbody>
-                        <tr v-for="info in characters_up" :key="info.lvl" class="dark:text-gray-100">
-                            <th class="border border-blue-800">{{ info.lvl }}</th>
-                            <th class="border border-blue-800">{{ info.books }}</th>
-                            <th class="border border-blue-800">{{ info.lostexp }}</th>
-                            <th class="border border-blue-800">{{ info.mora }}</th>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div> 
-        -->
+    <!-- Content -->
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-1">
 
-     <header class="bg-gray-50 shadow-lg mx-auto text-center max-w-lg mt-1 pt-2 rounded-lg dark:bg-gray-700 transition-colors">
-        <!-- Image loading -->
-            <vue-load-image>
-              <template v-slot:image>
-                  <img class="h-28 md:h-40 w-auto mx-auto rounded-xl mt-2" src="/img/paimon-work.gif" alt="paimon-work">
-              </template>
-              <template v-slot:preloader> 
-                  <div class="mt-2 w-28 h-28 mx-auto pt-8 md:h-40 md:pt-20">
-                    <svg class="mx-auto animate-spin text-spin text-gray-400 h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" id="spin"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
-                  </div>
-              </template>
-              <template v-slot:error>
-                  <div></div>
-              </template>
-          </vue-load-image>
-        <!-- /Image -->
-        <div class="max-w-7xl mx-auto pb-5 pt-4 px-3 sm:px-4 lg:px-5">
-            <h1 class="text-3xl font-bold leading-tight text-gray-900 dark:text-gray-200">Работаем...</h1>
-            <p class="dark:text-gray-300">В следующем обновлении будет</p>
-            <router-link to="/" class="inline-flex items-center justify-center mt-3 px-5 py-2 shadow-lg text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 dark:hover:bg-indigo-700 focus:outline-none">
-                На главную
-            </router-link>
+        <!-- Card1 -->
+        <div class="px-3 pb-2 rounded-lg mb-4 md:mb-8 overflow-hidden text-gray-900 dark:text-gray-200 shadow-lg bg-gray-50 dark:bg-gray-700 transition-colors sm:mx-4 justify-center">
+            <!-- Card-content -->
+            <h3 class="pb-2 pt-3 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:leading-10 dark:text-gray-200">
+                Прокачка героя
+            </h3>
+            
+            <div v-for="info in characters_up" :key="info.lvl">
+                <b class="text-lg">Уровень {{ info.lvl }}:</b>
+                <br>
+
+                Предметы:
+                <b v-for="n in 3" :key="n" class="inline-flex my-1">
+                    {{ info.books[n-1] }} 
+                        <!-- Icon loading -->
+                            <vue-load-image v-if="info.books[n-1]">
+                                <template v-slot:image>
+                                    <img class="h-6 w-6 mr-2" v :src="'/img/exp'+(4-n)+'.png'" alt="book-icon">
+                                </template>
+                                <template v-slot:preloader> 
+                                    <div class="h-4 w-4 m-1 mr-2 rounded-md bg-gray-200 dark:bg-gray-600 animate-pulse"></div>
+                                </template>
+                                <template v-slot:error>
+                                    <div class="h-4 w-4 m-1 mr-2 rounded-md bg-red-200 dark:bg-red-800 animate-pulse"></div>
+                                </template>
+                            </vue-load-image>
+                        <!-- /Icon -->
+                </b>
+                <br>
+
+                Потеряно EXP:
+                <b class="inline-flex my-1">
+                {{ info.lostexp }} 
+                    <!-- Icon loading -->
+                        <vue-load-image>
+                            <template v-slot:image>
+                                <img class="h-6 w-6 mr-2" src="/img/exp-icon.png" alt="exp-icon">
+                            </template>
+                            <template v-slot:preloader> 
+                                <div class="h-4 w-4 m-1 mr-2 rounded-md bg-gray-200 dark:bg-gray-600 animate-pulse"></div>
+                            </template>
+                            <template v-slot:error>
+                                <div class="h-4 w-4 m-1 mr-2 rounded-md bg-red-200 dark:bg-red-800 animate-pulse"></div>
+                            </template>
+                        </vue-load-image>
+                    <!-- /Icon -->
+                </b>
+                <br>
+
+                Стоимость:
+                <b class="inline-flex my-1">
+                {{ info.mora }} 
+                    <!-- Icon loading -->
+                        <vue-load-image>
+                            <template v-slot:image>
+                                <img class="h-6 w-6 mr-2" src="/img/mora-icon.png" alt="mora-icon">
+                            </template>
+                            <template v-slot:preloader> 
+                                <div class="h-4 w-4 m-1 mr-2 rounded-md bg-gray-200 dark:bg-gray-600 animate-pulse"></div>
+                            </template>
+                            <template v-slot:error>
+                                <div class="h-4 w-4 m-1 mr-2 rounded-md bg-red-200 dark:bg-red-800 animate-pulse"></div>
+                            </template>
+                        </vue-load-image>
+                    <!-- /Icon -->
+                </b>
+                <br><hr class="my-1 border-gray-200 dark:border-gray-500">
+
+            </div>
         </div>
-    </header>
+        <!-- /Card1 -->
+
+        <!-- Card2 -->
+        <div class="px-3 pb-2 rounded-lg mb-4 md:mb-8 overflow-hidden text-gray-900 dark:text-gray-200 shadow-lg bg-gray-50 dark:bg-gray-700 transition-colors sm:mx-4 justify-center">
+            <!-- Card-content -->
+            <h3 class="pb-2 pt-3 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:leading-10 dark:text-gray-200">
+                Расчет смолы
+            </h3>
+            
+            <div v-for="res in resin" :key="res.amount">
+                <div class="inline-flex w-full justify-between">
+                    <b class="text-lg inline-flex">0 -> {{ res.amount }}
+                        <!-- Icon loading -->
+                            <vue-load-image>
+                                <template v-slot:image>
+                                    <img class="h-6 w-6 mr-2 mt-0.5" src="/img/resin-icon.png" alt="resin-icon">
+                                </template>
+                                <template v-slot:preloader> 
+                                    <div class="h-4 w-4 m-1 mr-2 mt-1.5 rounded-md bg-gray-200 dark:bg-gray-600 animate-pulse"></div>
+                                </template>
+                                <template v-slot:error>
+                                    <div class="h-4 w-4 m-1 mr-2 mt-1.5 rounded-md bg-red-200 dark:bg-red-800 animate-pulse"></div>
+                                </template>
+                            </vue-load-image>
+                        <!-- /Icon -->
+                    </b>
+                    <b class="mr-2">{{ res.wait }} час.</b>
+                </div>
+                <br><hr class="my-2 border-gray-200 dark:border-gray-500">
+            </div>
+        </div>
+        <!-- /Card2 -->
+    </div>
 </template>
 
 
@@ -49,12 +114,31 @@ import VueLoadImage from 'vue-load-image'
 const characters_up = [
   { lvl: '0-20', books: [6, null, 1], lostexp: 825, mora: 24000 },
   { lvl: '20-40', books: [28, 3, 4], lostexp: 675, mora: 115600 },
+  { lvl: '40-50', books: [29, null, null], lostexp: 900, mora: 115800 },
+  { lvl: '50-60', books: [42, 3, null], lostexp: 875, mora: 170800 },
+  { lvl: '60-70', books: [59, 3, 1], lostexp: 75, mora: 239000 },
+  { lvl: '70-80', books: [80, 2, 2], lostexp: 125, mora: 322200 },
+  { lvl: '80-90', books: [171, 4, null], lostexp: 875, mora: 684600 }
+]
+
+const resin = [
+  { amount: 15, wait: 2 },
+  { amount: 30, wait: 4 },
+  { amount: 45, wait: 6 },
+  { amount: 60, wait: 8 },
+  { amount: 75, wait: 10 },
+  { amount: 90, wait: 12 },
+  { amount: 105, wait: 14 },
+  { amount: 120, wait: 16 },
+  { amount: 145, wait: 19 },
+  { amount: 160, wait: 21 }
 ]
 
 export default {
     setup() {
         return{
-            characters_up
+            characters_up,
+            resin
         }
     },
     components: {

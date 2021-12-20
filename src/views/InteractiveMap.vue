004e4ce0f3e9f28233e58d156a4544a9e03b6640
Oleg <scherbaoleg4@gmail.com>
2021-12-20 23:50:41 +0200

    improve map page

--- src/views/InteractiveMap.vue
+++ src/views/InteractiveMap.vue
@@ -1,10 +1,12 @@
 <template>
     <div>
-        <iframe title="interactive map" src="https://mapofgenshin.com/" class="absolute h-full min-w-full left-0 right-0 top-0 pt-14 md:pt-16 z-10" frameBorder="0" sandbox="allow-same-origin allow-scripts allow-popups allow-forms">
-            :(
-        </iframe>
-        <div class="absolute top-1/3 left-0 right-0 w-full z-0">
-            <svg class="mx-auto animate-spin text-gray-400 h-10 w-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" id="spin"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
+        <!-- Container + iframe -->
+        <div class="absolute top-0 left-0 right-0 h-full pt-18 mx-auto max-w-7xl px-4 sm:pt-20 sm:px-6 lg:pt-24 lg:px-8 xl:pt-26 pb-4 sm:pb-8">
+            <div class="h-full min-w-full rounded-lg mb-4 md:mb-8 shadow-lg bg-black transition-colors">
+                <iframe title="interactive map" src="https://mapofgenshin.com/" class="h-full w-full z-10 rounded-lg" frameBorder="0" sandbox="allow-same-origin allow-scripts allow-popups allow-forms">
+                    :(
+                </iframe>
+            </div>
         </div>
     </div>
 </template>

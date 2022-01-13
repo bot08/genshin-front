a000438bf04bc37ca0cea6776b301f9b8bf674c9
Oleg <scherbaoleg4@gmail.com>
2022-01-13 21:46:44 +0200

    buttons contrast increased & follow-redirects issue fix

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,12 +16,12 @@
                       type="button"
                       aria-expanded="false"
                       @click="themeSwitch"
-                      class="bg-white rounded-md p-2 ml-3 inline-flex items-center justify-center text-gray-400 hover:bg-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 dark:bg-gray-800">
+                      class="bg-white rounded-md p-2 ml-3 inline-flex items-center justify-center text-gray-500 dark:text-gray-300 hover:bg-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 dark:bg-gray-800">
                         <span class="sr-only">Theme</span>
                         <MoonIcon class="h-6 w-6 dark:hidden" aria-hidden="true"/>
                         <SunIcon class="h-6 w-6 hidden dark:block" aria-hidden="true"/>
                     </button>
-                    <PopoverButton class="bg-white rounded-md p-2 ml-3 inline-flex items-center justify-center text-gray-400 hover:bg-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 dark:bg-gray-800">
+                    <PopoverButton class="bg-white rounded-md p-2 ml-3 inline-flex items-center justify-center text-gray-500 dark:text-gray-300 hover:bg-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 dark:bg-gray-800">
                       <span class="sr-only">Open main menu</span>
                       <MenuIcon class="h-6 w-6" aria-hidden="true"/>
                     </PopoverButton>
@@ -53,7 +53,7 @@
                     <img class="h-8 w-auto" src="/favicon.svg" alt="logo" />
                   </div>
                   <div class="-mr-2">
-                    <PopoverButton class="bg-gray-50 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 dark:bg-gray-700">
+                    <PopoverButton class="bg-gray-50 rounded-md p-2 inline-flex items-center justify-center text-gray-500 dark:text-gray-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 dark:bg-gray-700">
                       <span class="sr-only">Close main menu</span>
                       <XIcon class="h-6 w-6" aria-hidden="true" />
                     </PopoverButton>
@@ -61,7 +61,7 @@
                 </div>
                 <div class="px-2 pt-2 pb-3 space-y-1">
                   <PopoverButton v-for="item in navigation" :key="item.name" class="block">
-                    <router-link v :to="item.href" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 dark:text-gray-200 dark:hover:text-gray-300 dark:hover:bg-gray-800">
+                    <router-link v :to="item.href" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 dark:text-gray-200">
                       <component :is="item.icon" class="inline h-5 w-5 text-indigo-600 mx-1 v-align-min3-5" aria-hidden="true" />
                       {{ item.name }}
                     </router-link>

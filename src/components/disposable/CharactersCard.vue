7566f989fca309392ccbe5339a85786ae582b435
Oleg <scherbaoleg4@gmail.com>
2022-02-02 11:55:23 +0200

    init

---
+++ src/components/disposable/CharactersCard.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="rounded-lg mt-1 mb-4 md:mb-8 overflow-hidden shadow-lg bg-gray-50 dark:bg-gray-700 transition-colors sm:mx-auto sm:text-center sm:w-60">
+        <router-link class="flex sm:block" v :to="'/characters/'+nameEngProps">
+            <!-- Image loading -->
+            <vue-load-image>
+                <template v-slot:image>
+                    <img class="w-28 h-28 sm:mx-auto rounded-xl mx-3 my-3 sm:mt-2 sm:mb-0 object-cover drop-shadow" v :src="'https://sushicat.pp.ua/api'+icoProps" alt="char-portret">
+                </template>
+                <template v-slot:preloader> 
+                    <div class="w-20 h-24 bg-gray-200 dark:bg-gray-600 sm:mx-auto rounded-xl mx-7 my-5 sm:mt-3 sm:mb-3 animate-pulse"></div>
+                </template>
+                <template v-slot:error>
+                    <div class="w-20 h-24 bg-red-200 dark:bg-red-800 sm:mx-auto rounded-xl mx-7 my-5 sm:mt-3 sm:mb-3 animate-pulse text-center">img</div>
+                </template>
+            </vue-load-image>
+            <!-- /Image -->
+            <div class="px-1 py-1">
+                <div class="font-bold mt-3 sm:mt-0 text-xl dark:text-gray-100">{{ nameProps }}</div>
+                <div class="text-lg mb-1 dark:text-gray-100">{{ rarityProps }}<StarIcon class="h-5 w-5 inline v-align-min3-5"/></div>
+            </div>
+        </router-link>
+    </div>
+</template>
+
+
+<script>
+import VueLoadImage from 'vue-load-image'
+import { StarIcon } from '@heroicons/vue/solid'
+
+
+export default {
+    name: "CharactersCard",
+
+    props:[
+        'nameProps',
+        'nameEngProps',
+        'icoProps',
+        'rarityProps'
+    ],
+
+    components: {
+    'vue-load-image': VueLoadImage,
+    StarIcon
+  },
+}
+</script>
--- src/views/Characters.vue
+++ src/views/Characters.vue
@@ -15,27 +15,7 @@
 
   <!--Card-->
   <div v-else class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-    <div v-for="item in characters" :key="item.name" class="rounded-lg mt-1 mb-4 md:mb-8 overflow-hidden shadow-lg bg-gray-50 dark:bg-gray-700 transition-colors sm:mx-auto sm:text-center sm:w-60">
-        <router-link class="flex sm:block" v :to="'/characters/'+item.nameeng">
-            <!-- Image loading -->
-            <vue-load-image>
-                <template v-slot:image>
-                    <img class="w-28 h-28 sm:mx-auto rounded-xl mx-3 my-3 sm:mt-2 sm:mb-0 object-cover drop-shadow" v :src="'https://sushicat.pp.ua/api'+item.ico.path" alt="char-portret">
-                </template>
-                <template v-slot:preloader> 
-                    <div class="w-20 h-24 bg-gray-200 dark:bg-gray-600 sm:mx-auto rounded-xl mx-7 my-5 sm:mt-3 sm:mb-3 animate-pulse"></div>
-                </template>
-                <template v-slot:error>
-                    <div class="w-20 h-24 bg-red-200 dark:bg-red-800 sm:mx-auto rounded-xl mx-7 my-5 sm:mt-3 sm:mb-3 animate-pulse text-center">img</div>
-                </template>
-            </vue-load-image>
-            <!-- /Image -->
-            <div class="px-1 py-1">
-                <div class="font-bold mt-3 sm:mt-0 text-xl dark:text-gray-100">{{ item.name }}</div>
-                <div class="text-lg mb-1 dark:text-gray-100">{{ item.rarity }}<StarIcon class="h-5 w-5 inline v-align-min3-5"/></div>
-            </div>
-        </router-link>
-    </div>
+    <CharactersCard v-for="item in characters" :key="item.name" :nameProps="item.name" :nameEngProps="item.nameeng" :rarityProps="item.rarity" :icoProps="item.ico.path"/>
   </div>
 
   <!-- Server error -->
@@ -45,10 +25,9 @@
 
 <script>
 import axios from 'axios'
-import VueLoadImage from 'vue-load-image'
-import { StarIcon } from '@heroicons/vue/solid'
 import SelectMenu from '@/components/SelectMenu.vue'
 import Error from '@/components/Error.vue'
+import CharactersCard from '@/components/disposable/CharactersCard.vue'
 
 // Менюшка выбора
 const sort = [
@@ -63,10 +42,9 @@ const sort = [
 export default {
 
   components: {
-    'vue-load-image': VueLoadImage,
-    StarIcon,
     Error,
-    SelectMenu
+    SelectMenu,
+    CharactersCard
   },
 
   data: () => ({

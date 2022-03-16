2635a115bc18a11c64843e8ae697f1696f23af51
Oleg <scherbaoleg4@gmail.com>
2022-03-16 15:31:35 +0200

    Update characters.js

--- functions/characters.js
+++ functions/characters.js
@@ -1,5 +1,4 @@
-const fetch = require("node-fetch");
-
+import { fetch } from "node-fetch"
 const API_ENDPOINT = "https://api-genshin.herokuapp.com/api/characters";
 
 exports.handler = async (event, context) => {

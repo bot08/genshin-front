c265b8db68367c26d4b4863cdf8fd79d3680abcb
Oleg <scherbaoleg4@gmail.com>
2022-04-13 20:32:14 +0300

    update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
-import html from 'vite-plugin-html'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 
 export default defineConfig({
   plugins: [
     vue(),
-    html({
+    createHtmlPlugin({
       minify: true,
+      entry: '/src/main.js',
     }),
   ],
   resolve: {

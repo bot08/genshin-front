966a83ca95a1f3dbacbf5a42a8b740eb2d801fc2
Oleg <scherbaoleg4@gmail.com>
2022-06-29 21:30:30 +0300

    change user agent

--- api/img.js
+++ api/img.js
@@ -1,11 +1,16 @@
 import fetch from "node-fetch"
 const API_ROOT ='https://sushicat.pp.ua/api/genshin/storage/uploads/2021/07/15/Character_Tartaglia_Portrait-1_uid_60f0b8383d496.png';
 
+module.exports = (url, args = {}) => {
+    args.headers = args.headers || {}
+    args.headers['user-agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59'
+    return fetch(url, args)
+  }
 
 exports.handler = async (event, context) => {
             try {
                 const doggoEndpoint =`${API_ROOT}`;
-                const response = await fetch(doggoEndpoint, {headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59' }});
+                const response = await fetch(doggoEndpoint);
                 const images = await response.json();
                 return {
                     statusCode: 200,

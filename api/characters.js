97a4ea740f4caa782dabf3ac2bf55f0217f53c40
Oleg <scherbaoleg4@gmail.com>
2022-06-29 13:33:03 +0300

    add headers to request

--- api/characters.js
+++ api/characters.js
@@ -1,10 +1,12 @@
 import fetch from "node-fetch"
-const API_ENDPOINT = "https://api-genshin.herokuapp.com/api/characters";
+const API_ENDPOINT = "https://sushicat.pp.ua/api/genshin/api/collections/get/charactersv2?token=a4191046104f8f3674f788e804c2d0";
 
 exports.handler = async (event, context) => {
   try {
-      const response = await
-      fetch(API_ENDPOINT);
+      const response = await fetch(API_ENDPOINT, {
+                                                method: 'GET',
+                                                headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59' }}
+                                                );
       const data = await response.json();
       return {
           statusCode: 200,

d68e421f2fe1b6ca0cbe4d6d889dec9ca9b2d26c
Oleg <scherbaoleg4@gmail.com>
2022-07-25 01:01:04 +0300

    update dev content

--- src/views/Dev/Index.vue
+++ src/views/Dev/Index.vue
@@ -7,7 +7,7 @@
           <!-- Image loading -->
             <vue-load-image>
               <template v-slot:image>
-                  <img class="h-72 sm:h-96 m-auto rounded-xl mt-4 drop-shadow" src="/img/paimon-portrait2.png" alt="paimon-portret">
+                  <img @click="easterEgg = !easterEgg" class="h-72 sm:h-96 m-auto rounded-xl mt-4 drop-shadow" :src="easterEgg ? '/img/ken.png' : '/img/paimon-portrait2.png'" alt="paimon-portret">
               </template>
               <template v-slot:preloader> 
                   <div class="h-72 sm:h-96 mx-auto sm:mt-4 mb-4 sm:mb-0 pt-28">
@@ -44,7 +44,7 @@
           <a target="_blank"  rel="noreferrer" href="https://github.com/JQweenq/Genshin" class="items-center m-2 text-center px-7 py-2 shadow-lg text-base font-medium leading-6 text-white transition ease-in-out bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 dark:hover:bg-indigo-700 focus:outline-none">
               Android app (github)
           </a>
-          <a target="_blank"  rel="noreferrer" href="https://github.com/JQweenq/api.genshin-journey.ml" class="items-center m-2 text-center px-7 py-2 shadow-lg text-base font-medium leading-6 text-white transition ease-in-out bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 dark:hover:bg-indigo-700 focus:outline-none">
+          <a target="_blank"  rel="noreferrer" href="https://github.com/JQweenq/genshin-back" class="items-center m-2 text-center px-7 py-2 shadow-lg text-base font-medium leading-6 text-white transition ease-in-out bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 dark:hover:bg-indigo-700 focus:outline-none">
               Open API (github)
           </a>
           <a target="_blank"  rel="noreferrer" href="https://github.com/bot08/matross-uikit" class="items-center m-2 text-center px-7 py-2 shadow-lg text-base font-medium leading-6 text-white transition ease-in-out bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 dark:hover:bg-indigo-700 focus:outline-none">
@@ -65,6 +65,10 @@ import LazyLinkBtn from '@/components/LazyLinkBtn.vue'
 export default {
   // Сохранение состояния (keep-alive)
   name: "notAlive",
+
+  data: () => ({
+    easterEgg: false
+  }),
 
   components: {
     'vue-load-image': VueLoadImage,
